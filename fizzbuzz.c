280657174b64d4612083374c15c83fa0cd2aab80
Ricardo Riveron <larius828@yahoo.com>
2017-07-31 20:02:48 -0500

    More friendly code with 1 less import and a functional buffer. Cannot reduce code any further.

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 int main() {
 
+	char buffer[8];
 	int foo = 3;
 	int bar = 5;
 
 	for(int i = 1; i < 101; i++) {
-
-		char buffer[8];
-
-	 	if (i%foo == 0){strcpy(buffer,"Fizz");}
-	 	if (i%bar == 0){strcpy(buffer,"Buzz");}
-	 	if (strcmp(buffer,"") == 0){itoa(i,buffer,10);}
-
-	 	printf("%s\n",buffer);
+		strcpy(buffer,"");
+	 	if (i%foo == 0){strcat(buffer,"Fizz");}
+	 	if (i%bar == 0){strcat(buffer,"Buzz");}
+	 	if (strcmp(buffer,"") == 0){printf("%i\n",i);}
+	 	else{printf("%s\n",buffer);}	 	
 	}
 	return 0;
 }
