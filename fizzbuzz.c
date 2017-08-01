9b7d93cc88cd1da98aad3169786718e88dcdaf1a
Ricardo Riveron <larius828@yahoo.com>
2017-07-31 19:18:15 -0500

    Attempt #1, works, but looks ugly and repeats a check.

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -2,6 +2,22 @@
 
 main() {
 
- printf("Hello World\n");
+	for(int i = 1; i < 101; i++) {
+		char fizz[] = "Fizz";
+		char buzz[] = "Buzz";
+		char fizzBuzz[] = "FizzBuzz";
 
+	 	if (i%3 == 0){
+	 		if (i%5 == 0){
+	 			printf("%s\n",fizzBuzz);
+	 		}else{
+	 			printf("%s\n",fizz);
+	 		}
+	 	}else if(i%5 == 0){
+	 		printf("%s\n",buzz);
+	 	}else {
+	 		printf("%i\n",i);
+	 	}
+	}
+	return 0;
 }
